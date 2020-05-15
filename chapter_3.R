873fefc61a0675c9a9ad38e2f40d8aabfc5ff222
Axel Wollan <wollan.axel@gmail.com>
2020-05-15 22:02:28 +0200

    adds 2 files

---
+++ chapter_3.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+
+data_mpg <- as_tibble(mpg)
+
+head(mpg)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = class, y = drv))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color = class))
+
+ggplot(data=mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, colour = displ<5 | hwy<20), show.legend = FALSE)
+
+ggplot(data = mpg)+
+  geom_point(mapping = aes(displ, hwy)) +
+  facet_wrap(~class, nrow = 2)
+
+ggplot(data = mpg)+
+  geom_point(mapping = aes(displ, hwy)) +
+  facet_grid(drv~cyl)
+
+small <- subset(mtcars, cyl == 4)
+big <- subset(mtcars, cyl > 4)
+
+
+ggplot(mtcars, aes(mpg, wt, colour = factor(cyl))) +
+  geom_point()+
+  lims()
+
